a8bacc387f8f55b2af49ac0d99e1ad465030c5ea
Usman Kamili <kamili0uk@gmail.com>
2022-08-24 09:57:22 +0530

    Added comments to tests
    Added comments to tests

--- src/test/java/CodingAssignment/AssignmentTest/AssignmentTestWithJsonServer.java
+++ src/test/java/CodingAssignment/AssignmentTest/AssignmentTestWithJsonServer.java
@@ -12,40 +12,53 @@ import io.restassured.specification.ResponseSpecification;
 
 public class AssignmentTestWithJsonServer {
 	
+	//RequestSpecification for mock JSON server
 	RequestSpecification request = new RequestSpecBuilder().setBaseUri("http://localhost:3000/teamRCB").build();
+	//ResponseSpecification for mock JSON server
 	ResponseSpecification response = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
 
 	@Test
 	public void validateTeamHasFourForeignPlayers()
 	{
+		//Get response from JSON server and get response in POJO class also verifies 200 status code
 		TeamRCB[] rcb = given().spec(request).expect().defaultParser(Parser.JSON)
 				.when().get()
 				.then().spec(response).extract()
 				.as(TeamRCB[].class);
 		int totalForeignPlayers = 0;
+		
+		//Loop to get total foreign players in the team
 		for(int i=0;i< rcb[0].getPlayer().size();i++)
 		{			
 			if(!rcb[0].getPlayer().get(i).getCountry().equalsIgnoreCase("India"))
 				totalForeignPlayers = totalForeignPlayers + 1;		
 		}
+		
+		//Assertion to validate if the number of foreign players in the team are 4
 		Assert.assertEquals(totalForeignPlayers , 4 , "Foreign players should not be more than 4");
 	}
 	
 	@Test
 	public void validateTeamHasAtleastOneWicketKeeper()
 	{		
+		//Get response from JSON server and get response in POJO class also verifies 200 status code
+
 		TeamRCB[] rcb = given().spec(request).expect().defaultParser(Parser.JSON)
 				.when().get()
 				.then().spec(response).extract()
 				.as(TeamRCB[].class);
 		int totalWicketKeeper = 0;
+		
+		//Loop to get total wicket keepers in the team
 		for(int i=0;i< rcb[0].getPlayer().size();i++)
 		{
 			if(rcb[0].getPlayer().get(i).getRole().equalsIgnoreCase("Wicket-keeper"))
 				totalWicketKeeper = totalWicketKeeper + 1;
 		
 		}
-		Assert.assertTrue(totalWicketKeeper>0 , "Total number of wicket keepers in the team are more than 1"); 
+		
+		//Assertion to validate if the number of wicket keepers in the team are atleast 1
+		Assert.assertTrue(totalWicketKeeper>0 , "Total number of wicket keepers in the team are atleast 1"); 
 		
 		
 	
--- src/test/java/CodingAssignment/AssignmentTest/AssignmentTestsWithJson.java
+++ src/test/java/CodingAssignment/AssignmentTest/AssignmentTestsWithJson.java
@@ -9,6 +9,7 @@ import io.restassured.path.json.JsonPath;
 public class AssignmentTestsWithJson {
 	JsonPath teamRcbJson; 
 	
+	// BeforeSuite hook to get JSON by using JsonPath before the execution of all the tests
 	@BeforeSuite
 	public void BeforeExecution()
 	{
@@ -19,11 +20,15 @@ public class AssignmentTestsWithJson {
 	public void validateTeamHasFourForeignPlayers()
 	{
 		int totalForeignPlayers = 0;
+		
+		//Loop to get total foreign players in the team
 		for(int i=0;i< teamRcbJson.getInt("player.size()");i++)
 		{			
 			if(!teamRcbJson.get("player["+i+"].country").toString().equalsIgnoreCase("India"))
 				totalForeignPlayers = totalForeignPlayers + 1;		
 		}
+		
+		//Assertion to validate if the number of foreign players in the team are 4
 		Assert.assertEquals(totalForeignPlayers , 4 , "Foreign players should not be more than 4");
 	}
 	
@@ -31,11 +36,15 @@ public class AssignmentTestsWithJson {
 	public void validateTeamHasAtleastOneWicketKeeper()
 	{
 		int totalWicketKeeper = 0;
+		
+		//Loop to get total wicket keepers in the team
 		for(int i=0;i< teamRcbJson.getInt("player.size()");i++)
 		{			
 			if(teamRcbJson.get("player["+i+"].role").toString().equalsIgnoreCase("Wicket-keeper"))
 				totalWicketKeeper = totalWicketKeeper + 1;		
 		}
+		
+		//Assertion to validate if the number of wicket keepers in the team are atleast 1
 		Assert.assertTrue(totalWicketKeeper>0 , "Total number of wicket keepers in the team are more than 1"); 
 	}
 
